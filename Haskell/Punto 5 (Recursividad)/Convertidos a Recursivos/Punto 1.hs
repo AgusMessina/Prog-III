c6001ba353e88f5cc8e64d49bf8809409748a79f
gusgus <73510218+AgusMessina@users.noreply.github.com>
2025-08-22 08:21:49 -0300

    Delete Haskell/Punto 5 (Recursividad)/Convertidos a Recursivos/Punto 1.hs

--- Haskell/Punto 5 (Recursividad)/Convertidos a Recursivos/Punto 1.hs
+++
@@ -1,5 +0,0 @@
-:{
-contar :: [a] -> Int
-contar [] = 0       -- Caso base: Si la lista esta vacia, devuelve 0
-contar (_:return) = 1 + contar return  -- Caso Recursivo: Vamos ignorando el primer elemento de la lista sucesivamente hasta llegar a una lista vacia, para despues empezar a sumarle uno por uno hasta terminar la ejecucion escalada
-:}
