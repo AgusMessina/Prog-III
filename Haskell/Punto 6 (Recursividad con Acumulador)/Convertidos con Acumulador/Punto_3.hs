584ff5f4d3c4029ad727b9a00449f378dac7c832
gusgus <73510218+AgusMessina@users.noreply.github.com>
2025-08-24 17:09:21 -0300

    Update Punto_3.hs

--- Haskell/Punto 6 (Recursividad con Acumulador)/Convertidos con Acumulador/Punto_3.hs
+++ Haskell/Punto 6 (Recursividad con Acumulador)/Convertidos con Acumulador/Punto_3.hs
@@ -1,8 +1,7 @@
 module Punto_3 where
 
-encontrar elemento lista = fAux 0 elemento lista where
-    fAux :: Eq a => a -> a -> [a] -> Bool 
-    fAux acum _ [] = False
-    fAux _(_:_) = True
-    fAux acum elemento (x:xs)   | elemento == x = fAux (acum + 1) elemento xs 
-	                            | otherwise = fAux acum elemento xs 
+encontrar elemento lista = fAux False elemento lista where
+    fAux :: Eq a => Bool -> a -> [a] -> Bool 
+    fAux acum _ [] = acum
+    fAux acum elemento (x:xs)   | elemento == x = fAux True elemento xs 
+                                | otherwise = fAux acum elemento xs 
